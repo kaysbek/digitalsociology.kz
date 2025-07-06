5abccaa50d119d2d8b04ac0c2ef4a3e5ee379669
kaysbek <kaysbek@gmail.com>
2025-07-07 02:28:33 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
-import Navbar from '@/components/Navbar'
+// pages/_app.js
+import '../styles/globals.css'
+import Navbar from '../components/Navbar'
 
 export default function App({ Component, pageProps }) {
   return (
